0965b3b6ed97cc4b156a9776fef16e8322b4089b
szymon.borucki@gmail.com <szymon.borucki@gmail.com>
2019-04-22 11:19:47 +0200

    update benchmark results

--- app/parser_benchmark.rb
+++ app/parser_benchmark.rb
@@ -16,7 +16,10 @@ Benchmark.bm do |benchmark|
 end
 
 # RESULT
-# [#<Benchmark::Tms:0x007fffc120ad50 @label="threads 1", @real=0.9997770000009041, @cstime=0.0, @cutime=0.0, @stime=0.23000000000000398, @utime=0.1599999999999966, @total=0.39000000000000057>, #<Benchmark::Tms:0x007fcf04a45710 @label="threads 4", @real=0.72076399999969, @cstime=0.0, @cutime=0.0, @stime=0.22999999999998977, @utime=0.3199999999999932, @total=0.549999999999983>, #<Benchmark::Tms:0x007fce7729d018 @label="threads 10", @real=0.567892999999458, @cstime=0.0, @cutime=0.0, @stime=0.27000000000001023, @utime=0.2400000000000091, @total=0.5100000000000193>, #<Benchmark::Tms:0x007fffc14127d8 @label="threads 100", @real=0.54185200000029, @cstime=0.0, @cutime=0.0, @stime=0.1599999999999966, @utime=0.25, @total=0.4099999999999966>]
+# @label="threads 1", @real=0.9997770000009041
+# @label="threads 4", @real=0.72076399999969
+# @label="threads 10", @real=0.567892999999458
+# @label="threads 100", @real=0.54185200000029
 
 
 Benchmark.bm do |benchmark|
